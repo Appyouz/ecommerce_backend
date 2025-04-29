a777a416aff947c1514ab38c937cd41d1bf731a4
appyouz <dikpalkhatri7@gmail.com>
2025-04-29 17:59:46 +0545

    Refactor: django-rest-auth(depreciated), replaced with: dj-rest-auth

--- ecommerce_backend/settings.py
+++ ecommerce_backend/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'rest_framework_simplejwt',
-    'rest_auth',
+    'dj_rest_auth',
     'accounts.apps.AccountsConfig',
 ]
 
