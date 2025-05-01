1eb24838fcfa8897d6f49719e0cc9da77164fcfd
appyouz <dikpalkhatri7@gmail.com>
2025-05-01 12:58:37 +0545

    Feat: Custome login and logout view, and urls path

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import Home
+from .views import Home, CustomLoginView, CustomLogoutView
 
 urlpatterns = [
     path('', Home.as_view()),
+    path('login/', CustomLoginView.as_view(), name='rest_login'),
+    path('logout/', CustomLogoutView.as_view(), name='rest_logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,11 @@
-from rest_framework.permissions import IsAuthenticated
+from typing import override
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework import status
+from .serialzers import UserSerializer
+from dj_rest_auth.views import LoginView, LogoutView
 
 
 class Home(APIView):
@@ -11,3 +15,18 @@ class Home(APIView):
     def get(self ):
         content = {'message': 'Hello world'}
         return Response(content)
+
+class CustomLoginView(LoginView):
+    permission_classes = [AllowAny]
+    
+    @override
+    def get_response(self):
+        return super().get_response()
+        user = self.user
+        if user and user.is_authenticated:
+            response.data['user'] = UserSerializer(user).data
+
+        return response
+
+class CustomLogoutView(LogoutView):
+    pass
