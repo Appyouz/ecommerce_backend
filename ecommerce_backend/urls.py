c1586b1748e6f1939a4301d43adf6f4a1b68fe33
appyouz <dikpalkhatri7@gmail.com>
2025-05-01 22:42:12 +0545

    Refactor: removed logout urls

--- ecommerce_backend/urls.py
+++ ecommerce_backend/urls.py
@@ -11,7 +11,6 @@ urlpatterns = [
 
      path('dj-rest-auth/login/', CustomLoginView.as_view(), name='rest_login'),
 
-     path('dj-rest-auth/logout/', CustomLogoutView.as_view(), name='rest_logout'),
 
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
