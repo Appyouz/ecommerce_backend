36b36161155f29ffdaa542ce054f1e65b95849ce
appyouz <dikpalkhatri7@gmail.com>
2025-05-03 13:36:21 +0545

    Feat: category model in products app

---
+++ products/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+    # * - [ ] Define `Product` model (fields: name, description, price, stock,
+     #       image_url, category - create Category model too)
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
