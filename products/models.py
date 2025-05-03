005edd9232565d7ca0641d2a28e33da003b9fa4c
appyouz <dikpalkhatri7@gmail.com>
2025-05-03 13:57:46 +0545

    Feat: product models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,5 @@
+from unicodedata import category
+from django.core.validators import MinValueValidator
 from django.db import models
 
     # * - [ ] Define `Product` model (fields: name, description, price, stock,
@@ -7,7 +9,7 @@ class Category(models.Model):
     name = models.CharField(max_length=200, unique=True)
     description = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name_plural = 'categories'
@@ -17,3 +19,22 @@ class Category(models.Model):
         return self.name
 
 
+class Product(models.Model):
+    name = models.CharField(max_length=200);
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        validators=[MinValueValidator(0.1)]
+    )
+    stock = models.PositiveIntegerField(default=0)
+    image_url = models.URLField(blank=True, null=True)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='products'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
