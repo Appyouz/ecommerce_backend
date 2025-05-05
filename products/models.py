af32e1269e6e1ebb82597eaf333eddbd58c14311
appyouz <dikpalkhatri7@gmail.com>
2025-05-05 14:53:41 +0545

    MISC: Removed todo list from models.py

--- products/models.py
+++ products/models.py
@@ -1,9 +1,6 @@
 from django.core.validators import MinValueValidator
 from django.db import models
 
-    # * - [ ] Define `Product` model (fields: name, description, price, stock,
-     #       image_url, category - create Category model too)
-
 class Category(models.Model):
     name = models.CharField(max_length=200, unique=True)
     description = models.TextField(blank=True, null=True)
