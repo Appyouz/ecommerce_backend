582b546bdd0c7766198d6f4bb05e7356f1c6b959
appyouz <dikpalkhatri7@gmail.com>
2025-05-05 15:25:24 +0545

    FEAT: main urls addition for products app

--- ecommerce_backend/urls.py
+++ ecommerce_backend/urls.py
@@ -15,5 +15,6 @@ urlpatterns = [
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('', include('accounts.urls')),
+    path('api/', include('products.urls')),
 ]
 
