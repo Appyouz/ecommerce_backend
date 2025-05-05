96828ed0c8bd7e6f5eb50a0199693aed0c249c2e
appyouz <dikpalkhatri7@gmail.com>
2025-05-05 15:23:49 +0545

    Refactor: formatting

--- products/serializers.py
+++ products/serializers.py
@@ -19,3 +19,9 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = ['id', 'name', 'description', 'price', 'stock', 'category', 'image', 'created_at', 'updated_at']
         read_only_fields = ('created_at', 'updated_at')
+
+    # price validation
+    def validate_price(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Price must be positive.")
+        return value
