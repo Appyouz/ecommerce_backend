b07890fc0c27e4986777101fde4da238ce3004c1
appyouz <dikpalkhatri7@gmail.com>
2025-05-07 00:11:29 +0545

    FEAT: include necessary coniguration to serve medai files

--- ecommerce_backend/urls.py
+++ ecommerce_backend/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
-from accounts.views import CustomLoginView, CustomLogoutView 
+from accounts.views import CustomLoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -18,3 +18,9 @@ urlpatterns = [
     path('api/', include('products.urls')),
 ]
 
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
