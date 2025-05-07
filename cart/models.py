817f6b05fd02dcb77a1a2dbcb966f92dd40534c3
appyouz <dikpalkhatri7@gmail.com>
2025-05-07 13:59:35 +0545

    FEAT: cart and cartItem models

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from products.models import Product
+from django.core.validators import MinValueValidator
+
+User = settings.AUTH_USER_MODEL
+
+class Cart(models.Model):
+     
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='cart')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Cart of {self.user.username}"
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveBigIntegerField(default=1, validators=[MinValueValidator(1)])
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        # Ensure a product can only appear once in a specific cart(prevents duplicate entries)
+        unique_together = ('cart', 'product')
+        ordering = ['created_at']
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} in {self.cart.user.username}'s cart"
+
