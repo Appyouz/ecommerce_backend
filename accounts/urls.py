927949d0f51ad29a77584c76578c145bf07041fc
appyouz <dikpalkhatri7@gmail.com>
2025-05-10 12:05:14 +0545

    FEAT: urls for get-token

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import Home, CustomLoginView, CustomLogoutView
+from .views import Home, CustomLoginView, CustomLogoutView, GetAccessTokenView
 
 urlpatterns = [
     path('', Home.as_view()),
     # path('login/', CustomLoginView.as_view(), name='rest_login'),
     # path('logout/', CustomLogoutView.as_view(), name='rest_logout'),
+
+    path('auth/get-token/', GetAccessTokenView.as_view(), name='get-access-token'),
 ]
