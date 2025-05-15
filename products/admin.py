da77cae4a3385ae1d03b39d2f5c85255f9c197a6
appyouz <dikpalkhatri7@gmail.com>
2025-05-15 17:57:58 +0545

    FEAT: custom productAdmin and categoryAdmin implemented

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,28 @@
 from django.contrib import admin
 from .models import Product, Category
 
-admin.site.register(Category)
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'stock', 'created_at', 'updated_at')
+
+    list_filter = ('category', 'created_at', 'updated_at')
+
+    search_fields = ('name', 'description', 'category__name') # Use '__' to search related fields
+
+
+    date_hierarchy = 'created_at'
+
+    ordering = ('-created_at',) # Order by creation date, newest first
+
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'created_at', 'updated_at')
+
+    search_fields = ('name', 'description')
+
+    ordering = ('name',) # Order categories alphabetically by name
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
