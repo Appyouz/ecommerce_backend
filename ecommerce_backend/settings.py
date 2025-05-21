d86bfe252eaed176754f2eed9cccd0e475d49d10
appyouz <dikpalkhatri7@gmail.com>
2025-05-21 13:05:35 +0545

    INSTALLED: django_filters :

--- ecommerce_backend/settings.py
+++ ecommerce_backend/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'django_filters',
 
     'crispy_forms',
     'crispy_bootstrap4',
